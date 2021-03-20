14befeb70becde571dda26a3152bd02c91babc52
Gina A <ginadzani@gmail.com>
2021-03-20 20:07:45 +0100

    Add numbers on carousel's images, change arrow style

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -2,30 +2,29 @@ import React from 'react';
 
 export default class Carrousel extends React.Component {
 	render() {
+		const carouselLength = this.props.pictures.length
+
 		return (
 			<div id="carousel-main" className="carousel slide" data-interval="false">
-
-				<ol className="carousel-indicators">
-					{this.props.pictures.map((picture, index) => (
-						<li data-target="#carousel-main" data-slide-to={index} className={index === 0 ? "active" : ""} key={index}></li>
-					))}
-				</ol>
 
 				<div className="carousel-inner">
 					{this.props.pictures.map((picture, index) => (
 						<div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
 							<img className="carousel-img" src={picture} alt="property to rent" />
+							<div className="carousel-caption">
+								<p>{index+1}/{carouselLength}</p>
+							</div>
 						</div>
 					))}
 				</div>
 
 				<a className="carousel-control-prev" href="#carousel-main" role="button" data-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true" />
+					<i className="fa fa-angle-left carousel-arrow" />
 					<span className="sr-only">Previous</span>
 				</a>
 
 				<a className="carousel-control-next" href="#carousel-main" role="button" data-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true" />
+					<i className="fa fa-angle-right carousel-arrow" />
 					<span className="sr-only">Next</span>
 				</a>
 
