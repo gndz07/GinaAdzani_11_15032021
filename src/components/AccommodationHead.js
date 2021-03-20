075959c207200e7d29e68af6db76a26c6f9bb3ed
Gina A <ginadzani@gmail.com>
2021-03-20 19:37:54 +0100

    Add star rating

--- src/components/AccommodationHead.js
+++ src/components/AccommodationHead.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 export default class AccommodationHead extends React.Component {
+	fillStar = (rate) => {
+		const fullStar = 5;
+		const starPercentage = (rate / fullStar) * 100;
+		return starPercentage+"%";
+	}
+
 	render() {
+		const starWidth = this.fillStar(this.props.item.rating)
+		const starStyle = {
+			width: starWidth,
+		}
+
 		return (
 			<div id="accommodation-head">
 				<div id="accommodation-head-left">
@@ -16,7 +27,9 @@ export default class AccommodationHead extends React.Component {
 				</div>
 
 				<div id="owner">
-					<p>star rating</p>
+					<div className="stars-outer">
+          				<div className="stars-inner" style={starStyle}></div>
+        			</div>
 					<div id="accommodation-owner">
 						<p id="accommodation-owner-name">{this.props.item.host.name}</p>
 						<img src={this.props.item.host.picture} alt={this.props.item.host.name} id="accommodation-owner-photo" />
