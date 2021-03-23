7f247efe2d6419bd5b19faf503206ec05580e4d5
Gina A <ginadzani@gmail.com>
2021-03-23 10:10:42 +0100

    Move router to one file

---
+++ src/apps/Router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
+import Homepage from '../pages/Homepage.js'
+import About from '../pages/About.js'
+import Error from '../pages/Error.js'
+import Accommodation from '../pages/Accommodation.js'
+import '../styles/header.css'
+import '../styles/footer.css'
+
+export default class KasaRouter extends React.Component {
+	render() {
+		return (
+			<Router>
+    			<div>
+		    		<Header />
+		    		<Switch>
+		    			<Route exact path='/' component={Homepage} />
+		    			<Route path='/about' component={About} />
+		    			<Route path='/accommodation/:accommodationId' component={Accommodation} />
+		    			<Route component={Error} />
+		    		</Switch>
+		    		<Footer />
+		    	</div>
+    		</Router>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header.js'
-import Footer from './components/Footer.js'
-import Homepage from './pages/Homepage.js'
-import About from './pages/About.js'
-import Error from './pages/Error.js'
-import Accommodation from './pages/Accommodation.js'
-import './styles/header.css'
-import './styles/footer.css'
+import KasaRouter from './apps/Router.js'
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-    	<div>
-    		<Header />
-    		<Switch>
-    			<Route exact path='/' component={Homepage} />
-    			<Route path='/about' component={About} />
-    			<Route path='/accommodation/:accommodationId' component={Accommodation} />
-    			<Route component={Error} />
-    		</Switch>
-    		<Footer />
-    	</div>
-    </Router>
+    <KasaRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
