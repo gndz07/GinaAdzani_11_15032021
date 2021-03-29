f8ae7f90067904a39e402e3d22a6909ba179bd1e
Gina A <ginadzani@gmail.com>
2021-03-29 12:16:24 +0200

    Hide arrow when there's only one image

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -4,31 +4,50 @@ export default class Carrousel extends React.Component {
 	render() {
 		const carouselLength = this.props.pictures.length
 
-		return (
-			<div id="carousel-main" className="carousel slide" data-interval="false">
+		if (carouselLength > 1) {
+			return (
+				<div id="carousel-main" className="carousel slide" data-interval="false">
 
-				<div className="carousel-inner">
-					{this.props.pictures.map((picture, index) => (
-						<div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
-							<img className="carousel-img" src={picture} alt="property to rent" />
-							<div className="carousel-caption">
-								<p>{index+1}/{carouselLength}</p>
+					<div className="carousel-inner">
+						{this.props.pictures.map((picture, index) => (
+							<div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
+								<img className="carousel-img" src={picture} alt="property to rent" />
+								<div className="carousel-caption">
+									<p>{index+1}/{carouselLength}</p>
+								</div>
 							</div>
-						</div>
-					))}
+						))}
+					</div>
+
+					<a className="carousel-control-prev" href="#carousel-main" role="button" data-slide="prev">
+						<i className="fa fa-angle-left carousel-arrow" />
+						<span className="sr-only">Previous</span>
+					</a>
+
+					<a className="carousel-control-next" href="#carousel-main" role="button" data-slide="next">
+						<i className="fa fa-angle-right carousel-arrow" />
+						<span className="sr-only">Next</span>
+					</a>
+
 				</div>
+			)
+		} else {
+			return (
+				<div id="carousel-main" className="carousel slide" data-interval="false">
 
-				<a className="carousel-control-prev" href="#carousel-main" role="button" data-slide="prev">
-					<i className="fa fa-angle-left carousel-arrow" />
-					<span className="sr-only">Previous</span>
-				</a>
-
-				<a className="carousel-control-next" href="#carousel-main" role="button" data-slide="next">
-					<i className="fa fa-angle-right carousel-arrow" />
-					<span className="sr-only">Next</span>
-				</a>
-
-			</div>
-		)
+					<div className="carousel-inner">
+						{this.props.pictures.map((picture, index) => (
+							<div className={`carousel-item ${index === 0 ? "active" : ""}`} key={index}>
+								<img className="carousel-img" src={picture} alt="property to rent" />
+								<div className="carousel-caption">
+									<p>{index+1}/{carouselLength}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			)
+		}
+	
 	}
 }
